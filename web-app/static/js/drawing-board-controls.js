103a59352c38e0b852454c333039e709f8d70679
Mayank_MP5 <mayank5pande@gmail.com>
2020-06-08 12:23:00 +0530

    Playing with Drawingboard

---
+++ web-app/static/js/drawing-board-controls.js
@@ -0,0 +1,23 @@
+var simpleBoard = new DrawingBoard.Board("simple-board", {
+  controls: false,
+  background: "#000",
+  color: "#fff",
+  size: 20,
+  webStorage: false,
+});
+//simpleBoard.addControl("Download"); //if the DrawingBoard.Control.Download class exists
+
+//listen to an event
+//simpleBoard.ev.bind("board:reset", why);
+
+//stop listening to it
+
+//simpleBoard.ev.unbind("board:reset", why);
+
+function why() {
+  alert("OH GOD WHY");
+}
+
+const ResetBG = () => {
+  simpleBoard.reset();
+};
