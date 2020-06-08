5c9a39efd21ad9b500c3309068026afba7fa9ad2
Mayank_MP5 <mayank5pande@gmail.com>
2020-06-08 16:25:31 +0530

    Image URI Issues

--- web-app/app.py
+++ web-app/app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request, flash, redirect
 import os
+import numpy as np
+import base64
+import re
+
 
 app = Flask(__name__)
 
@@ -23,10 +27,20 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
-        if 'file1' not in request.files:
+        print(request.files)
+        if 'imagedata' not in request.files:
             return 'there is no file1 in form!'
         file1 = request.files['file1']
         path = os.path.join(app.config['UPLOAD_FOLDER'], file1.filename)
         file1.save(path)
+        print("Image sent to UPLOADS ...")
         return path
         return 'ok'
+
+
+@app.route('/hook', methods=['POST'])
+def get_image():
+    image_b64 = request.values['imageBase64']
+    image_data = re.sub('^data:image/.+;base64,', '',
+                        image_b64).decode('base64')
+    return 'All Okkk'
