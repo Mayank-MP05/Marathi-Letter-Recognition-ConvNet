61d9b401b5d87815589e8542895aef942b86a010
Mayank_MP5 <mayank5pande@gmail.com>
2020-06-08 14:57:32 +0530

    Images for Reference Page

--- web-app/app.py
+++ web-app/app.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/ref')
+def ref():
+    return render_template('ref.html')
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
