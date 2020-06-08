2302e4ea8c6affb9aea69dcc0b9f4fc3cb0e4552
Mayank_MP5 <mayank5pande@gmail.com>
2020-06-08 15:19:32 +0530

    File Input Submission

--- web-app/app.py
+++ web-app/app.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect
+import os
+
 app = Flask(__name__)
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['png'])
 
 
 @app.route('/ref')
@@ -10,3 +15,18 @@ def ref():
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file1' not in request.files:
+            return 'there is no file1 in form!'
+        file1 = request.files['file1']
+        path = os.path.join(app.config['UPLOAD_FOLDER'], file1.filename)
+        file1.save(path)
+        return path
+        return 'ok'
